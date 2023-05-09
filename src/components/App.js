26d68ffe0c5a9d28a7d9293383ffb10a4b246cde
nikos papadakis <npapadakis@gmail.com>
2023-05-09 21:50:45 +0300

    Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,8 @@ import {
   loadExchange,
 } from "../store/interactions";
 
+import Navbar from "./Navbar";
+
 function App() {
   const dispatch = useDispatch();
 
@@ -20,8 +22,15 @@ function App() {
     // Fetch current network's chainId (hardhat: 31337)
     const chainId = await loadNetwork(provider, dispatch);
 
-    // Fetch current account & balance from Metamask
-    await loadAccount(provider, dispatch);
+    // Reload page when network changes
+    window.ethereum.on("chainChanged", () => {
+      window.location.reload();
+    });
+
+    // Fetch current account & balance from Metamask when changed
+    window.ethereum.on("accountsChanged", () => {
+      loadAccount(provider, dispatch);
+    });
 
     // SPH Token smart contract
     const SPH = config[chainId].SPH;
@@ -39,7 +48,7 @@ function App() {
 
   return (
     <div>
-      {/* Navbar */}
+      {<Navbar />}
 
       <main className="exchange grid">
         <section className="exchange__section--left grid">

eb6722a85131808245d19c3a052af48392b3926d
nikos papadakis <npapadakis@gmail.com>
2023-05-12 15:58:00 +0300

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("solidity-coverage");
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
   solidity: "0.8.17",
   networks: {
-    localhost: {},
+    hardhat: {
+      chainId: 31337,
+      // gasPrice: 130000000000,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+      blockConfirmations: 6,
+    },
   },
 };
